1db89528d9b19de52143ec4f2db2fc0c3a89de23
Ayush Rawat <rawat@akeo.no>
2021-02-17 20:51:09 +0530

    WIP: paginated table

--- src/views/Pages/POIs.js
+++ src/views/Pages/POIs.js
@@ -1,169 +1,114 @@
 import React, { useEffect, useState } from 'react';
 import { axios_auth } from '../../api';
-import {
-	Badge,
-	Button,
-	Card,
-	Form,
-	InputGroup,
-	Navbar,
-	Nav,
-	Table,
-	OverlayTrigger,
-	Tooltip,
-	Container,
-	Row,
-	Col,
-  } from "react-bootstrap";
+import Table from '../../components/ReactTable/ReactTable'
+
+
 export const POIs = () => {
-	const [poisData, setPoisData] = useState();
-	const fetchPoisData = async () => {
-		const res = await axios_auth.get('/services/services/api/ese-pois');
-		console.log('fetchPoisData -> res', res.data);
-		if (res.data) {
-			setPoisData(res.data);
-		}
-		else {
-			alert("Error fetching data");
-		}
-	};
+  const [poisData, setPoisData] = useState();
+  const fetchPoisData = async () => {
+    const res = await axios_auth.get('/services/services/api/ese-pois');
+    console.log('fetchPoisData -> res', res.data);
+    if (res.data) {
+      setPoisData(res.data);
+    }
+    else {
+      alert("Error fetching data");
+    }
+  };
 
-	useEffect(() => {
-		fetchPoisData();
-	}, []);
+  useEffect(() => {
+    fetchPoisData();
+  }, []);
 
-	return (
-	<div>
-		<Container fluid>
-        <Row>
-          <Col md="12">
-            <Card className="card-plain table-plain-bg">
-              <Card.Header>
-                <Card.Title as="h4">POIs</Card.Title>
-                {/* <p className="card-category">
-                  Here is a subtitle for this table
-                </p> */}
-              </Card.Header>
-              <Card.Body className="table-full-width">
-                <Table>
-                  <thead>
-                    <tr>
-                      <th>ID</th>
-                      <th>Holding No</th>
-                      <th>Plot No</th>
-                      <th>Owner Name</th>
-                      <th>Guardian Name</th>
-                      <th>Mobile No</th>
-                      <th>Address 1</th>
-                      <th>Address 2</th>
-                      <th>Address 3</th>
-                      <th>Ward</th>
-                      <th>Landmark</th>
-                      <th>Latitude</th>
-                      <th>Longitude</th>
-                      <th>QR Code</th>
-                      <th>RFID Code</th>
-                      <th>Additional Detail</th>
-                      <th>Email</th>
-                      <th>Reason for followup</th>
-                      <th>Notes</th>
-                      <th>Pin Code</th>
-                      <th>POI Type</th>
-                      <th>POI Property</th>
-                      <th className="text-right">Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-					  {
-						  poisData && poisData.length > 0 &&
-						  	poisData.map((poi, index) => {
-								return (<tr>
-                      				<td>{poi.id}</td>
-                      				<td>{poi.holdingNo}</td>
-                      				<td>{poi.plotNo}</td>
-                      				<td>{poi.ownerName}</td>
-                      				<td>{poi.guardianName}</td>
-                      				<td>{poi.mobileNo}</td>
-                      				<td>{poi.address1}</td>
-                      				<td>{poi.address2}</td>
-                      				<td>{poi.address3}</td>
-                      				<td>{poi.eseWardWardDesc}</td>
-                      				<td>{poi.landMark}</td>
-                      				<td>{poi.latitude}</td>
-                      				<td>{poi.longitude}</td>
-                      				<td>{poi.qrCode}</td>
-                      				<td>{poi.rfidCode}</td>
-                      				<td>{poi.additionalDetails}</td>
-                      				<td>{poi.email}</td>
-                      				<td>{poi.reasonForFollowUp}</td>
-                      				<td>{poi.notes}</td>
-                      				<td>{poi.esePinCodePinCode}</td>
-                      				<td>{poi.esePoiTypePoiTypeName}</td>
-                      				<td>{poi.esePoiPropertyUsageTypePropertyUsageTypeName}</td>
-									  <td className="td-actions text-right">
-                        <OverlayTrigger
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                          overlay={
-                            <Tooltip id="tooltip-48903503">
-                              View Profile..
-                            </Tooltip>
-                          }
-                        >
-                          <Button
-                            className="btn-link btn-xs"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                            variant="info"
-                          >
-                            <i className="fas fa-user"></i>
-                          </Button>
-                        </OverlayTrigger>
-                        <OverlayTrigger
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                          overlay={
-                            <Tooltip id="tooltip-981231696">
-                              Edit Profile..
-                            </Tooltip>
-                          }
-                        >
-                          <Button
-                            className="btn-link btn-xs"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                            variant="success"
-                          >
-                            <i className="fas fa-edit"></i>
-                          </Button>
-                        </OverlayTrigger>
-                        <OverlayTrigger
-                          href="#pablo"
-                          onClick={(e) => e.preventDefault()}
-                          overlay={
-                            <Tooltip id="tooltip-255158527">Remove..</Tooltip>
-                          }
-                        >
-                          <Button
-                            className="btn-link btn-xs"
-                            href="#pablo"
-                            onClick={(e) => e.preventDefault()}
-                            variant="danger"
-                          >
-                            <i className="fas fa-times"></i>
-                          </Button>
-                        </OverlayTrigger>
-                      </td>
-								</tr>)
-							  })
-					  }
-                  </tbody>
-                </Table>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-	</div>
-	)
+  return (
+    poisData && poisData.length > 0 ? <Table columns={[
+      {
+        Header: "ID",
+        accessor: "id"
+      },
+      {
+        Header: "Holding number",
+        accessor: "holdingNo"
+      },
+      {
+        Header: "plotNo",
+        accessor: "plotNo"
+      },
+      {
+        Header: "eseWardWardDesc",
+        accessor: "eseWardWardDesc"
+      },
+      {
+        Header: "guardianName",
+        accessor: "guardianName"
+      },
+      {
+        Header: "address1",
+        accessor: "address1"
+      },
+      {
+        Header: "address2",
+        accessor: "address2"
+      },
+      {
+        Header: "Mobile",
+        accessor: "mobileNo"
+      },
+      {
+        Header: "Owner name",
+        accessor: "ownerName"
+      },
+      {
+        Header: "landMark",
+        accessor: "landMark"
+      },
+      {
+        Header: "latitude",
+        accessor: "latitude"
+      },
+      {
+        Header: "longitude",
+        accessor: "longitude"
+      },
+      {
+        Header: "qrCode",
+        accessor: "qrCode"
+      },
+      {
+        Header: "rfidCode",
+        accessor: "rfidCode"
+      },
+      {
+        Header: "additionalDetails",
+        accessor: "additionalDetails"
+      },
+      {
+        Header: "email",
+        accessor: "email"
+      },
+      {
+        Header: "reasonForFollowUp",
+        accessor: "reasonForFollowUp"
+      },
+      {
+        Header: "notes",
+        accessor: "notes"
+      },
+      {
+        Header: "esePinCodePinCode",
+        accessor: "esePinCodePinCode"
+      },
+      {
+        Header: "esePoiTypePoiTypeName",
+        accessor: "esePoiTypePoiTypeName"
+      },
+      {
+        Header: "esePoiPropertyUsageTypePropertyUsageTypeName",
+        accessor: "esePoiPropertyUsageTypePropertyUsageTypeName"
+      }
+    ]}
+
+      data={poisData}
+    /> : <div></div>
+  )
 };

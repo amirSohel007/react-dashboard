8dc1a2fda0c0fdc5051839b0a502c82af7d5bbda
Amir Sohel <amirsohel27@gmail.com>
2021-02-18 23:41:17 +0530

    logout action added

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,25 +1,36 @@
 import React from 'react';
-
 // react-bootstrap components
 import {
-	Badge,
 	Button,
-	ButtonGroup,
-	Card,
-	Dropdown,
+
+
+
+
+
+
+
+
+	Container, Dropdown,
 	Form,
 	InputGroup,
-	Navbar,
-	Nav,
-	Pagination,
-	Container,
-	Row,
-	Col,
-	Collapse,
+
+	Nav, Navbar
 } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { LOGOUT } from '../../store/actions/actionTypes';
+
+
 
 function AdminNavbar() {
 	const [collapseOpen, setCollapseOpen] = React.useState(false);
+	const dispatch = useDispatch();
+	const history = useHistory()
+
+	const handleSignOut = () => {
+		dispatch({ type: LOGOUT})
+		history.push('/auth/login-page')
+	}
 	return (
 		<>
 			<Navbar expand='lg'>
@@ -137,7 +148,7 @@ function AdminNavbar() {
 									<Dropdown.Item
 										className='text-danger'
 										href='#pablo'
-										onClick={(e) => e.preventDefault()}>
+										onClick={handleSignOut}>
 										<i className='nc-icon nc-button-power'></i>
 										Log out
 									</Dropdown.Item>
